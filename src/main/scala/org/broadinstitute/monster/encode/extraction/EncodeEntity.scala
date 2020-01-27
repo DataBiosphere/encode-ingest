c5af6a366ff26f3b459e6776451063ea62a941e9
raaidbroad <52425631+raaidbroad@users.noreply.github.com>
2020-01-27 15:21:00 -0500

    Ra dspdc 642 start new pipeline (#3)
    * works for limit=25, does not work if limit=all
    
    * add logback.xml, update coder so things don't break, add todos
    
    * clean up code a bit
    
    * address some of the PR comments
    
    * few more PR comments addressed, added first pass at replicates and experiments
    
    * address PR comments
    
    * change limit back to limit=all
    
    * address additional PR comments; move transform statements higher up in pipeline

---
+++ src/main/scala/org/broadinstitute/monster/encode/extraction/EncodeEntity.scala
@@ -0,0 +1,29 @@
+package org.broadinstitute.monster.encode.extraction
+
+import enumeratum.{Enum, EnumEntry}
+
+import scala.collection.immutable
+
+/**
+  * An entity type found in ENCODE that we also want
+  * (with modifications) in our data model.
+  */
+sealed trait EncodeEntity extends EnumEntry
+
+object EncodeEntity extends Enum[EncodeEntity] {
+  case object Biosample extends EncodeEntity
+
+  case object Donor extends EncodeEntity
+
+  case object Experiment extends EncodeEntity
+
+  case object File extends EncodeEntity
+
+  case object Library extends EncodeEntity
+
+  case object Replicate extends EncodeEntity
+
+  case object Audit extends EncodeEntity
+
+  override def values: immutable.IndexedSeq[EncodeEntity] = findValues
+}
---
+++ src/main/scala/org/broadinstitute/monster/encode/extraction/EncodeExtractions.scala
@@ -0,0 +1,210 @@
+package org.broadinstitute.monster.encode.extraction
+
+import java.io.{IOException, InputStream, OutputStream}
+import java.nio.ByteBuffer
+
+import com.google.common.io.ByteStreams
+import com.spotify.scio.coders.Coder
+import com.spotify.scio.transforms._
+import com.spotify.scio.values.SCollection
+import com.squareup.okhttp.{Callback, OkHttpClient, Request, Response}
+import io.circe.JsonObject
+import io.circe.jawn.JawnParser
+import io.circe.syntax._
+import org.apache.beam.sdk.coders.{Coder => BeamCoder}
+import org.apache.beam.sdk.coders.{KvCoder, StringUtf8Coder}
+import org.apache.beam.sdk.transforms.{GroupIntoBatches, ParDo}
+import org.apache.beam.sdk.util.VarInt
+import org.apache.beam.sdk.values.KV
+
+import scala.collection.JavaConverters._
+import scala.concurrent.{Future, Promise}
+
+/** Ingest step responsible for pulling raw metadata for a specific entity type from the ENCODE API. */
+object EncodeExtractions {
+
+  // TODO when updating to upack and msg, will need to change this coder as well
+  implicit val jsonCoder: Coder[JsonObject] = Coder.beam {
+    new BeamCoder[JsonObject] {
+      val parser = new JawnParser()
+
+      override def encode(value: JsonObject, outStream: OutputStream): Unit =
+        Option(value).foreach { jsonObj =>
+          val bytes = jsonObj.asJson.noSpaces.getBytes
+          VarInt.encode(bytes.length, outStream)
+          outStream.write(bytes)
+        }
+      override def decode(inStream: InputStream): JsonObject = {
+        val numBytes = VarInt.decodeInt(inStream)
+        val bytes = new Array[Byte](numBytes)
+        ByteStreams.readFully(inStream, bytes)
+        parser
+          .decodeByteBuffer[JsonObject](ByteBuffer.wrap(bytes))
+          .fold(throw _, identity)
+      }
+      override def getCoderArguments: java.util.List[_ <: BeamCoder[_]] =
+        java.util.Collections.emptyList()
+      override def verifyDeterministic(): Unit =
+        throw new BeamCoder.NonDeterministicException(
+          this,
+          "Equal Msgs don't necessarily encode to the same bytes"
+        )
+    }
+  }
+
+  /** HTTP client to use for querying ENCODE APIs. */
+  val client = new OkHttpClient()
+
+  /**
+    * Pipeline stage which maps batches of query params to output payloads
+    * by sending the query params to the ENCODE search API.
+    *
+    * @param encodeEntity the type of ENCODE entity the stage should query
+    */
+  class EncodeLookup(encodeEntity: EncodeEntity)
+      extends ScalaAsyncLookupDoFn[List[(String, String)], String, OkHttpClient] {
+
+    private val baseParams =
+      List("frame=object", "status=released", "limit=all", "format=json")
+
+    override def asyncLookup(
+      client: OkHttpClient,
+      params: List[(String, String)]
+    ): Future[String] = {
+      val paramStrings = params.map {
+        case (key, value) =>
+          s"$key=$value"
+      }
+      val allParams = s"type=${encodeEntity.entryName}" :: baseParams ::: paramStrings
+
+      get(client, allParams)
+    }
+
+    /**
+      * Construct a future which will either complete with the stringified
+      * payload resulting from querying a url, or fail.
+      *
+      * @param client the HTTP client to use in the query
+      * @param params ???
+      */
+    private def get(client: OkHttpClient, params: List[String]): Future[String] = {
+      val url = s"https://www.encodeproject.org/search/?${params.mkString(sep = "&")}"
+      val promise = Promise[String]()
+
+      val request = new Request.Builder()
+        .url(url)
+        .get
+        .build
+
+      client
+        .newCall(request)
+        .enqueue(new Callback {
+          override def onFailure(request: Request, e: IOException): Unit = {
+            promise.failure(e)
+            ()
+          }
+
+          override def onResponse(response: Response): Unit = {
+            if (response.isSuccessful) {
+              promise.success(response.body.string)
+            } else if (response.code() == 404) {
+              promise.success("""{ "@graph": [] }""")
+            } else {
+              promise.failure(
+                new RuntimeException(s"ENCODE lookup failed: $response")
+              )
+            }
+            ()
+          }
+        })
+
+      promise.future
+    }
+
+    override protected def newClient(): OkHttpClient = client
+  }
+
+  /**
+    * Pipeline stage which maps batches of search parameters into JSON entities
+    * from ENCODE matching those parameters.
+    *
+    * @param encodeEntity the type of ENCODE entity the stage should query
+    */
+  def getEntities(
+    encodeEntity: EncodeEntity
+  ): SCollection[List[(String, String)]] => SCollection[JsonObject] =
+    _.applyKvTransform(ParDo.of(new EncodeLookup(encodeEntity))).flatMap { kv =>
+      kv.getValue.fold(
+        throw _,
+        value => {
+          val decoded = for {
+            // TODO change to upack and msg instead of circe and json, dear future monster
+            json <- io.circe.parser.parse(value)
+            cursor = json.hcursor
+            objects <- cursor.downField("@graph").as[Vector[JsonObject]]
+          } yield {
+            objects
+          }
+          decoded.fold(throw _, identity)
+        }
+      )
+    }
+
+  /**
+    * Pipeline stage which extracts IDs from downloaded JSON entities for
+    * use in subsequent queries.
+    *
+    * @param referenceField field in the input JSONs containing the IDs
+    *                       to extract
+    * @param manyReferences whether or not `referenceField` is an array
+    */
+  def getIds(
+    referenceField: String,
+    manyReferences: Boolean
+  ): SCollection[JsonObject] => SCollection[String] =
+    collection =>
+      collection.flatMap { jsonObj =>
+        jsonObj(referenceField).toIterable.flatMap { referenceJson =>
+          val references = for {
+            refValues <- if (manyReferences) {
+              referenceJson.as[List[String]]
+            } else {
+              referenceJson.as[String].map { reference =>
+                List(reference)
+              }
+            }
+          } yield {
+            refValues
+          }
+          references.toOption
+        }.flatten
+      }.distinct
+
+  /**
+    * Pipeline stage which maps entity IDs into corresponding JSON entities
+    * downloaded from ENCODE.
+    *
+    * @param encodeEntity the type of ENCODE entity the input IDs correspond to
+    * @param batchSize the number of elements in a batch stream
+    * @param fieldName the field name to get the entity by, is "@id" by default
+    */
+  def getEntitiesByField(
+    encodeEntity: EncodeEntity,
+    batchSize: Long,
+    fieldName: String = "@id"
+  ): SCollection[String] => SCollection[JsonObject] = { idStream =>
+    val paramsBatchStream =
+      idStream
+        .map(KV.of("key", _))
+        .setCoder(KvCoder.of(StringUtf8Coder.of(), StringUtf8Coder.of()))
+        .applyKvTransform(GroupIntoBatches.ofSize(batchSize))
+        .map(_.getValue)
+        .map { ids =>
+          ids.asScala.foldLeft(List.empty[(String, String)]) { (acc, ref) =>
+            (fieldName -> ref) :: acc
+          }
+        }
+
+    getEntities(encodeEntity)(paramsBatchStream)
+  }
+}
