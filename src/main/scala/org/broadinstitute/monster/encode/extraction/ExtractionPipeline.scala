aa94e03ce3b42af32dd873fa0f2f3b1ddaccf6c4
Raaid Arshad <rarshad@broadinstitute.org>
2020-02-28 11:09:22 -0500

    pass EncodeClient to the pipeline builder in the extraction pipeline

--- src/main/scala/org/broadinstitute/monster/encode/extraction/ExtractionPipeline.scala
+++ src/main/scala/org/broadinstitute/monster/encode/extraction/ExtractionPipeline.scala
@@ -4,5 +4,6 @@ import org.broadinstitute.monster.common.{PipelineBuilder, ScioApp}
 
 /** Entry-point for the Encode pipeline's Docker image. */
 object ExtractionPipeline extends ScioApp[Args] {
-  override def pipelineBuilder: PipelineBuilder[Args] = ExtractionPipelineBuilder
+  override def pipelineBuilder: PipelineBuilder[Args] =
+    new ExtractionPipelineBuilder(EncodeClient.apply)
 }
