7f7f0566808440d289a9ae389e303d2d9c25d765
raaidbroad <52425631+raaidbroad@users.noreply.github.com>
2020-03-03 17:09:00 -0500

    Ra dspdc 831 accept client (#14)
    * update to use new helper code
    
    * create a val for the version number so that version updates bump in lockstep
    
    * remove empty test I accidentally added
    
    * start making updates, create EncodeClient
    
    * merge changes from master
    
    * pass EncodeClient to the pipeline builder in the extraction pipeline
    
    * move encodeExtractions into pipeline builder, delete old file
    
    * update existing unit test
    
    * fix reference to moved static function
    
    * add comments
    
    * make pipelineBuilder serializable
    
    * address PR comments
    
    * address additional PR comments

--- build.sbt
+++ build.sbt
@@ -1,10 +1,13 @@
 val enumeratumVersion = "1.5.13"
 
+val okhttpVersion = "4.4.0"
+
 lazy val `encode-ingest` = project
   .in(file("."))
   .enablePlugins(MonsterScioPipelinePlugin)
   .settings(
     libraryDependencies ++= Seq(
       "com.beachape" %% "enumeratum" % enumeratumVersion,
+      "com.squareup.okhttp3" % "okhttp" % okhttpVersion
     )
   )
---
+++ src/main/scala/org/broadinstitute/monster/encode/extraction/EncodeClient.scala
@@ -0,0 +1,72 @@
+package org.broadinstitute.monster.encode.extraction
+
+import java.io.IOException
+import java.time.Duration
+
+import okhttp3.{Call, Callback, OkHttpClient, Request, Response}
+import org.broadinstitute.monster.common.msg.JsonParser
+import org.slf4j.LoggerFactory
+import upack.{Arr, Msg, Obj, Str}
+
+import scala.concurrent.{Future, Promise}
+
+/** Interface for clients that hits ENCODE API. */
+trait EncodeClient extends Serializable {
+  def get(entity: EncodeEntity, params: List[(String, String)]): Future[Msg]
+}
+
+object EncodeClient {
+  private val timeout = Duration.ofSeconds(60)
+
+  private val baseParams =
+    List("frame=object", "status=released", "limit=all", "format=json")
+
+  def apply(): EncodeClient = {
+    val logger = LoggerFactory.getLogger(getClass)
+
+    val client = new OkHttpClient.Builder()
+      .connectTimeout(timeout)
+      .readTimeout(timeout)
+      .build()
+
+    (entity, params) => {
+      val paramStrings = params.map {
+        case (key, value) =>
+          s"$key=$value"
+      }
+      val allParams = s"type=${entity.entryName}" :: baseParams ::: paramStrings
+      val url = s"https://www.encodeproject.org/search/?${allParams.mkString(sep = "&")}"
+      val p = Promise[Msg]()
+
+      val request = new Request.Builder()
+        .url(url)
+        .get
+        .build
+
+      logger.debug(s"New API Query: [$request]")
+      client
+        .newCall(request)
+        .enqueue(new Callback {
+          override def onFailure(call: Call, e: IOException): Unit = {
+            p.failure(e)
+            ()
+          }
+
+          override def onResponse(call: Call, response: Response): Unit = {
+            if (response.isSuccessful) {
+              p.success(JsonParser.parseEncodedJson(response.body.string))
+            } else if (response.code() == 404) {
+              p.success(Obj(Str("@graph") -> Arr()))
+            } else {
+              p.failure(
+                new RuntimeException(s"ENCODE lookup failed: $response")
+              )
+            }
+            ()
+          }
+        })
+
+      p.future
+    }
+  }
+}
