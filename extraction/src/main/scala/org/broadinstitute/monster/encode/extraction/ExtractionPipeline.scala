5739892c1fb094e14b024d9514b4346b861a36bd
Quazi Hoque <qhoque@broadinstitute.org>
2020-03-10 12:09:54 -0400

    * Bugfix - Update ExtractionPipeline import to reflect new arg parser name

--- extraction/src/main/scala/org/broadinstitute/monster/encode/extraction/ExtractionPipeline.scala
+++ extraction/src/main/scala/org/broadinstitute/monster/encode/extraction/ExtractionPipeline.scala
@@ -1,6 +1,6 @@
 package org.broadinstitute.monster.encode.extraction
 
-import Args.customArgParser
+import Args.tupleParser
 import org.broadinstitute.monster.common.{PipelineBuilder, ScioApp}
 
 /** Entry-point for the Encode pipeline's Docker image. */
