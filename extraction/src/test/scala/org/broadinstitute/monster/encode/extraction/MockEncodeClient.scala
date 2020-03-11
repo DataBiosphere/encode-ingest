31bb5cf2c9ab7c267edc26e020b1fb18d9bac0d3
raaidbroad <52425631+raaidbroad@users.noreply.github.com>
2020-03-11 13:49:30 -0400

    Ra dspdc 237 extraction unit tests (#18)
    * split project into multi-project build
    
    * add skip publish step
    
    * started unit tests, created mock client
    
    * first unit test works (barring fcexperiment), writing test does not
    
    * first unit tests works entirely, including fcexperiment
    
    * disk writing test works now!
    
    * merged master into this, started cleaning up some code
    
    * add comment
    
    * remove using the 1 to 1 range
    
    * made a test case per encode entity, moved the test in ExtractionPipelineSpec to EncodeExtractionPipelineBuilderSpec and deleted the old file

---
+++ extraction/src/test/scala/org/broadinstitute/monster/encode/extraction/MockEncodeClient.scala
@@ -0,0 +1,25 @@
+package org.broadinstitute.monster.encode.extraction
+
+import upack.Msg
+
+import scala.concurrent.Future
+import scala.collection.mutable
+
+/** Mock interface for ENCODE API. */
+class MockEncodeClient(
+  responseMap: Map[(EncodeEntity, (String, String)), Msg]
+) extends EncodeClient {
+  val recordedRequests = mutable.Set[(EncodeEntity, (String, String))]()
+
+  override def get(entity: EncodeEntity, params: List[(String, String)]): Future[Msg] = {
+    val paramsToUse = params.head
+    recordedRequests.add((entity, paramsToUse))
+    responseMap
+      .get((entity, paramsToUse))
+      .fold(
+        Future.failed[Msg](
+          new RuntimeException(s"Entity: ${entity} and params: ${paramsToUse.toString}")
+        )
+      )(Future.successful)
+  }
+}
