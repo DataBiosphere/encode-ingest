5f892aa58d0aaa02ebb7210c2b3863b8d07c5fab
Raaid Arshad <rarshad@broadinstitute.org>
2020-03-11 13:43:09 -0400

    made a test case per encode entity, moved the test in ExtractionPipelineSpec to EncodeExtractionPipelineBuilderSpec and deleted the old file

--- extraction/src/test/scala/org/broadinstitute/monster/encode/extraction/EncodeExtractionPipelineBuilderSpec.scala
+++ extraction/src/test/scala/org/broadinstitute/monster/encode/extraction/EncodeExtractionPipelineBuilderSpec.scala
@@ -2,7 +2,7 @@ package org.broadinstitute.monster.encode.extraction
 
 import better.files.File
 import org.broadinstitute.monster.common.PipelineBuilderSpec
-import org.broadinstitute.monster.common.msg.MsgOps
+import org.broadinstitute.monster.common.msg.{JsonParser, MsgOps}
 import upack._
 
 import scala.collection.mutable
@@ -197,6 +197,57 @@ object EncodeExtractionPipelineBuilderSpec {
   )
 
   val mockClient = new MockEncodeClient(responseMap)
+
+  // examples to test isFunctionalCharacterizationReplicate
+  val fcExample =
+    JsonParser.parseEncodedJson(
+      """{
+        "@id": "/replicates/d80982c0-ae21-4e83-b3c9-fc7ec356c435/",
+        "@type": [
+          "Replicate",
+          "Item"
+        ],
+        "aliases": [
+          "tim-reddy:ggr-starrseq-input-rep11-rep"
+        ],
+        "biological_replicate_number": 11,
+        "date_created": "2019-11-07T21:22:51.002163+00:00",
+        "experiment": "/functional-characterization-experiments/ENCSR652MCH/",
+        "libraries": [
+           "ed595bdb-bab5-4b3b-acbf-221a131f4b4c"
+        ],
+        "library": "/libraries/ENCLB079TYN/",
+        "schema_version": "9",
+        "status": "released",
+        "submitted_by": "/users/a37a6376-c5a2-4fe0-b1af-7f078eb997c9/",
+        "technical_replicate_number": 1,
+        "uuid": "d80982c0-ae21-4e83-b3c9-fc7ec356c435"
+        }"""
+    )
+
+  val expExample =
+    JsonParser.parseEncodedJson(
+      """{
+      "@id": "/replicates/3c996486-0a1b-466c-be00-6d7cd7a89581/",
+      "@type": [
+        "Replicate",
+        "Item"
+      ],
+      "aliases": [],
+      "biological_replicate_number": 1,
+      "date_created": "2019-11-21T23:02:31.637903+00:00",
+      "experiment": "/experiments/ENCSR426KOP/",
+      "libraries": [
+        "f88d7865-b60b-418f-a0b1-0bf3fabc3c95"
+      ],
+      "library": "/libraries/ENCLB355PRN/",
+      "schema_version": "9",
+      "status": "released",
+      "submitted_by": "/users/bc5b62f7-ce28-4a1e-b6b3-81c9c5a86d7a/",
+      "technical_replicate_number": 1,
+      "uuid": "3c996486-0a1b-466c-be00-6d7cd7a89581"
+      }"""
+    )
 }
 
 class EncodeExtractionPipelineBuilderSpec extends PipelineBuilderSpec[Args] {
@@ -233,30 +284,106 @@ class EncodeExtractionPipelineBuilderSpec extends PipelineBuilderSpec[Args] {
     )
   }
 
-  it should "write downloaded outputs to disk" in {
+  it should "query and write Biosample data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.Biosample, biosampleParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.Biosample}/*.json") shouldBe
       biosampleOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write Donor data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.Donor, donorParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.Donor}/*.json") shouldBe
       donorOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write Library data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.Library, libraryParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.Library}/*.json") shouldBe
       libraryOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write Replicate data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.Replicate, replicateParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.Replicate}/*.json") shouldBe
       replicateOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write Antibody data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.AntibodyLot, antibodyParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.AntibodyLot}/*.json") shouldBe
       antibodyOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write Target data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.Target, targetParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.Target}/*.json") shouldBe
       targetOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write Experiment data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.Experiment, experimentParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.Experiment}/*.json") shouldBe
       experimentOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write FunctionalCharacterizationExperiment data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.FunctionalCharacterizationExperiment, fcExperimentParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.FunctionalCharacterizationExperiment}/*.json") shouldBe
       fcExperimentOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write File data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.File, fileParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.File}/*.json") shouldBe
       fileOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write AnalysisStepRun data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.AnalysisStepRun, analysisStepRunParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.AnalysisStepRun}/*.json") shouldBe
       analysisStepRunOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write AnalysisStepVersion data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.AnalysisStepVersion, analysisStepVersionParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.AnalysisStepVersion}/*.json") shouldBe
       analysisStepVersionOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "query and write AnalysisStep data as expected" in {
+    mockClient.recordedRequests.toSet should contain(
+      (EncodeEntity.AnalysisStep, analysisStepParams)
+    )
     readMsgs(outputDir, s"${EncodeEntity.AnalysisStep}/*.json") shouldBe
       analysisStepOut.read[Array[Msg]]("@graph").toSet
+  }
+
+  it should "Distinguish between normal experiments and functional-characterization-experiments" in {
+    //functional-characterization-experiments case
+    ExtractionPipelineBuilder.isFunctionalCharacterizationReplicate(fcExample) shouldBe true
+    //normal experiments case
+    ExtractionPipelineBuilder.isFunctionalCharacterizationReplicate(expExample) shouldBe false
   }
 }
