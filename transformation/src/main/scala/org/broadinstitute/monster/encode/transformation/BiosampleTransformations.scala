4f1fba98656a04efaa3f504cbff593b5aa4a9f13
Raaid Arshad <rarshad@broadinstitute.org>
2020-04-01 13:08:08 -0400

    done with dateObtained and donorId

--- transformation/src/main/scala/org/broadinstitute/monster/encode/transformation/BiosampleTransformations.scala
+++ transformation/src/main/scala/org/broadinstitute/monster/encode/transformation/BiosampleTransformations.scala
@@ -1,6 +1,6 @@
 package org.broadinstitute.monster.encode.transformation
 
-import java.time.OffsetDateTime
+import java.time.{LocalDate, OffsetDateTime}
 
 import org.broadinstitute.monster.encode.jadeschema.table.Biosample
 import upack.Msg
@@ -11,27 +11,24 @@ object BiosampleTransformations {
 
   /** Transform a raw ENCODE biosample into our preferred schema. */
   def transformBiosample(biosampleInput: Msg): Biosample = {
-
+    val (auditLevel, auditLabels) = CommonTransformations.summarizeAudits(biosampleInput)
     Biosample(
       id = CommonTransformations.readId(biosampleInput),
       crossReferences = biosampleInput.read[Array[String]]("dbxrefs"),
       timeCreated = biosampleInput.read[OffsetDateTime]("date_created"),
       source = biosampleInput.tryRead[String]("source"),
+      dateObtained = biosampleInput.tryRead[LocalDate]("date_obtained"),
       // DONE ABOVE HERE
-      // FIXME don't know what timeObtained maps to
-      timeObtained = "timestamp",
+
       derivedFromBiosampleId = biosampleInput.tryRead[String]("age_units"),
-      anatomicalSite = biosampleInput.tryRead[String]("biosample_ontology").flatMap(site =>
-      site.),
+      anatomicalSite = biosampleInput.tryRead[String]("biosample_ontology"),
       biosampleType = biosampleInput.tryRead[String]("biosample_ontology"),
       samplePreservationState = biosampleInput.tryRead[String](
         "preservation_method").fold("not reported")(identity), // DONE
       seeAlso = biosampleInput.tryRead[Array[String]]("url"), // DONE
-      donorIds = "",
-      auditLabels = biosampleInput.tryRead[Array[String]]("audit"), // DONE
-      maxAuditFlag = biosampleInput
-        .read[Array[String]]("parents")
-        .map(CommonTransformations.transformId),
+      donorId = biosampleInput.read[String]("donor"), // DONE
+      auditLabels = auditLabels, // DONE
+      maxAuditFlag = auditLevel, // DONE
       award = biosampleInput.tryRead[String]("award"), // DONE
       biologicalReplicateId = biosampleInput.tryRead[String]("submitted_by"),
       cellIsolationMethods = biosampleInput.tryRead[Array[String]]("cell_isolation_method"), // DONE
@@ -40,6 +37,7 @@ object BiosampleTransformations {
       lab = biosampleInput.tryRead[String]("lab"), // DONE
       replicationType = "",
       technicalReplicateId = "",
+
       // DONE BELOW HERE
       treatments = biosampleInput.read[Array[String]]("treatments"),
       wasPerturbed = biosampleInput.read[Boolean]("perturbed"),
