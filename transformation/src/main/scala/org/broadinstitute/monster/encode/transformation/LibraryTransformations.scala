5d5b9e44077efbb930d902aa44f97eba3799786d
Dan Moran <danmoran@broadinstitute.org>
2020-04-02 13:40:33 -0400

    Update library schema to match ENCODE changes.

--- transformation/src/main/scala/org/broadinstitute/monster/encode/transformation/LibraryTransformations.scala
+++ transformation/src/main/scala/org/broadinstitute/monster/encode/transformation/LibraryTransformations.scala
@@ -9,8 +9,16 @@ import upack.Msg
 object LibraryTransformations {
   import org.broadinstitute.monster.common.msg.MsgOps
 
+  /**
+    * Fallback value used by ENCODE for 'strand_specificity' in records that were submitted
+    * before they allowed for distinguishing between forward/reverse strands.
+    */
+  private val specificityPlaceholder = "strand-specific"
+
   /** Transform a raw ENCODE library into our preferred schema. */
-  def transformLibrary(libraryInput: Msg): Library =
+  def transformLibrary(libraryInput: Msg): Library = {
+    val specificity = libraryInput.tryRead[String]("strand_specificity")
+
     Library(
       id = CommonTransformations.readId(libraryInput),
       crossReferences = libraryInput.read[Array[String]]("dbxrefs"),
@@ -20,10 +28,12 @@ object LibraryTransformations {
       queriedRnpSizeRange = libraryInput.tryRead[String]("queried_RNP_size_range"),
       rnaIntegrityNumber = libraryInput.tryRead[Double]("rna_integrity_number"),
       sizeRange = libraryInput.tryRead[String]("size_range"),
-      strandSpecificity = libraryInput.read[Boolean]("strand_specificity"),
+      strandSpecific = specificity.isDefined,
+      strandSpecificity = specificity.filterNot(_ == specificityPlaceholder),
       treatments = libraryInput.read[Array[String]]("treatments"),
       submittedBy = libraryInput.read[String]("submitted_by"),
       spikeIns = libraryInput.read[Array[String]]("spikeins_used"),
       biosampleId = CommonTransformations.transformId(libraryInput.read[String]("biosample"))
     )
+  }
 }
