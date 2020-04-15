66f0b5c4cf97ef6258befe39a6c354fc395a2949
Daniel Moran <danmoran@broadinstitute.org>
2020-04-15 15:59:37 -0400

    DSPDC-846 Add library_prep generation to transformation pipeline. (#44)

--- transformation/src/main/scala/org/broadinstitute/monster/encode/transformation/TransformationPipelineBuilder.scala
+++ transformation/src/main/scala/org/broadinstitute/monster/encode/transformation/TransformationPipelineBuilder.scala
@@ -89,6 +89,7 @@ object TransformationPipelineBuilder extends PipelineBuilder[Args] {
     val libraryOutput = libraryInputs
       .withName("Transform libraries")
       .map(LibraryTransformations.transformLibrary)
+
     StorageIO.writeJsonLists(
       libraryOutput,
       "Libraries",
