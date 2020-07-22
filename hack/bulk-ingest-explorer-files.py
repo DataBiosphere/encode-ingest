c5dfb90432ebdff37c48c30b76e69e7f66b40778
Sarah Wessel <swessel@broadinstitute.org>
2020-07-22 11:49:17 -0400

    Initialize explorer file ingest script

---
+++ hack/bulk-ingest-explorer-files.py
@@ -0,0 +1,55 @@
+from collections import namedtuple
+import google.auth
+from google.auth.transport.requests import AuthorizedSession
+from requests.exceptions import HTTPError
+import sys
+
+# read arguments
+dataset_id = sys.argv[1]
+jade_base_url = sys.argv[2]
+
+credentials, project = google.auth.default(scopes=['openid', 'email', 'profile'])
+Counts = namedtuple('Counts', ['succeeded', 'failed', 'not_tried'])
+
+base_url = None # default to dev
+timeout = None # need to set value
+control_file_prefix = 'gs://broad-encode-migration-storage/explorer-backfill'
+control_file = control_file_prefix + "" # update to use file names
+max_failures = 1 # TODO: update - set to total file count (# lines in control file)
+profile_id = None # repo -> profile_id value (figure out where to get this?)
+load_tag = None # generate for each
+
+authed_session = AuthorizedSession(credentials)
+
+def submit_job(dataset_id: str, **kwargs):
+    response = authed_session.post(f'{jade_base_url}/api/repository/v1/datasets/{dataset_id}/files/bulk', json=kwargs)
+    if response.ok:
+        return response.json()['id']
+    else:
+        raise HTTPError(f'Bad response, got code of: {response.status_code}')
+
+def check_job_status(job_id: str):
+    response = authed_session.get(f"{jade_base_url}/api/repository/v1/jobs/{job_id}")
+    if response.ok:
+        return response.json()["job_status"]
+    else:
+        raise HTTPError("Bad response, got code of: {}".format(response.status_code))
+
+
+def is_done(job_id: str) -> bool:
+    # if "running" then we want to keep polling, so false
+    # if "succeeded" then we want to stop polling, so true
+    # if "failed" then we want to stop polling, so true
+    status = check_job_status(job_id)
+    return status in ["succeeded", "failed"]
+
+def is_success(job_id: str):
+    # need to spit out the lowercase strings instead of real bools, allows the workflow to know if it succeeded or not
+    if check_job_status(job_id) == "succeeded":
+        return "true"
+    else:
+        raise ValueError("Job ran but did not succeed.")
+
+# TODO query status until done
+
+# print(submit_job(dataset_id, profileId=profile_id, loadControlFile=control_file, loadTag=load_tag, maxFailedFileLoads=max_failures))
