cc1a280f29c6b4766e29ba9afd7dceace42ca557
Andrea Haessly <ahaessly@broadinstitute.org>
2022-08-22 16:37:32 -0400

    update missing file scripts

--- hack/check_for_missing_files.sh
+++ hack/check_for_missing_files.sh
@@ -8,7 +8,34 @@ fi
 PROJECT_ID=$1
 DATASET_NAME=$2
 
-JSON_LINE='{"file_id":"FILE","label":"FILE","xref":["https://www.encodeproject.org/files/FILE/"],"data_modality":[],"audit_labels":[],"file_type":"Unavailable","quality_metrics":[],"library_id":[],"uses_sample_biosample_id":[],"donor_id":[],"derived_from_file_id":[]}'
 
-MISSING_FILE_COUNT=`bq --project_id=${PROJECT_ID} --format=sparse query --dataset_id=${DATASET_NAME} -n 5000 --nouse_legacy_sql "SELECT count(distinct(alignment_used_file)) as MissingFileCount FROM alignmentactivity, UNNEST(used_file_id) alignment_used_file LEFT OUTER JOIN file AS file ON alignment_used_file=file_id WHERE file_id IS NULL"`
-echo Number of missing files ${MISSING_FILE_COUNT}
+function count_unnest() {
+  # $1 should be table name, $2 shoulde be field name
+  MISSING_FILE_SQL="SELECT count(distinct(linked_file)) as MissingFile FROM $1, UNNEST($2) linked_file WHERE linked_file NOT IN (SELECT file_id FROM file)"
+  NUM_MISSING=`bq --project_id=${PROJECT_ID} --format=sparse query --dataset_id=${DATASET_NAME} --nouse_legacy_sql "${MISSING_FILE_SQL}" | tail -n+3 | tr -d "[:blank:]"`
+  echo "Found ${NUM_MISSING} missing files from attribute $2 in table $1"
+}
+
+function count() {
+  MISSING_FILE_SQL="SELECT count(distinct($2)) FROM $1 WHERE $2 NOT IN (SELECT file_id FROM file)"
+  NUM_MISSING=`bq --project_id=${PROJECT_ID} --format=sparse query --dataset_id=${DATASET_NAME} --nouse_legacy_sql "${MISSING_FILE_SQL}" | tail -n+3 | tr -d "[:blank:]"`
+  echo "Found ${NUM_MISSING} missing files from attribute $2 in table $1"
+}
+
+# count_unnest "alignmentactivity" "generated_file_id"
+# count_unnest "alignmentactivity" "used_file_id"
+# count_unnest "analysisactivity" "derived_from_file_id"
+# count_unnest "analysisactivity" "generated_file_id"
+# count_unnest "assayactivity" "generated_file_id"
+# count_unnest "experimentactivity" "used_file_id"
+# count_unnest "experimentactivity" "generated_file_id"
+# count_unnest "file" "derived_from_file_id"
+# count_unnest "referencefileset" "derived_from_file_id"
+# count_unnest "referencefileset" "generated_file_id"
+# count_unnest "referencefileset" "original_file_id"
+# count_unnest "referencefileset" "related_file_id"
+# count_unnest "sequencingactivity" "generated_file_id"
+# count_unnest "stepactivity" "used_file_id"
+# count_unnest "stepactivity" "generated_file_id"
+
+count "file" "paired_with_file_id"
--- hack/create_missing_file_json.sh
+++ hack/create_missing_file_json.sh
@@ -15,37 +15,49 @@ rm ${TMP_FILE}
 
 JSON_LINE='{"file_id":"FILE","label":"FILE","xref":["https://www.encodeproject.org/files/FILE/"],"data_modality":[],"audit_labels":[],"file_type":"Unavailable","quality_metrics":[],"library_id":[],"uses_sample_biosample_id":[],"donor_id":[],"derived_from_file_id":[]}'
 
-
 function transform() {
-  # $1 should be table name, $2 shoulde be field name
-  MISSING_FILE_SQL="SELECT distinct(linked_file)  as MissingFile FROM $1, UNNEST($2) linked_file WHERE linked_file NOT IN (SELECT file_id FROM file)"
-  MISSING_FILE_IDS=`bq --project_id=${PROJECT_ID} --format=sparse query --dataset_id=${DATASET_NAME} -n 250000 --nouse_legacy_sql "${MISSING_FILE_SQL}" | tail -n+3 | tr -d "[:blank:]"`
+  # $1 should be the variable with the missing ids, $2 is the text to echo after the number of found files
   NUM_MISSING=0
   while IFS= read -r line; do
       if [ ! $line = "" ]; then
         NUM_MISSING=$((NUM_MISSING+1))
         echo "$JSON_LINE" | sed "s/FILE/$line/g" >> ${TMP_FILE}
       fi
-  done <<< "$MISSING_FILE_IDS"
-  echo "Found ${NUM_MISSING} missing files from attribute $2 in table $1"
+  done <<< "$1"
+  echo "Found ${NUM_MISSING} $2"
+
 }
 
-transform "alignmentactivity" "generated_file_id"
-transform "alignmentactivity" "used_file_id"
-transform "analysisactivity" "derived_from_file_id"
-transform "analysisactivity" "generated_file_id"
-transform "assayactivity" "generated_file_id"
-transform "experimentactivity" "used_file_id"
-transform "experimentactivity" "generated_file_id"
-transform "file" "derived_from_file_id"
-transform "file" "paired_with_file_id"
-transform "referencefileset" "derived_from_file_id"
-transform "referencefileset" "generated_file_id"
-transform "referencefileset" "original_file_id"
-transform "referencefileset" "related_file_id"
-transform "sequencingactivity" "generated_file_id"
-transform "stepactivity" "used_file_id"
-transform "stepactivity" "generated_file_id"
+function query_missing_unnest() {
+  # $1 should be table name, $2 shoulde be field name
+  MISSING_FILE_SQL="SELECT distinct(linked_file)  as MissingFile FROM $1, UNNEST($2) linked_file WHERE linked_file NOT IN (SELECT file_id FROM file)"
+  MISSING_FILE_IDS=`bq --project_id=${PROJECT_ID} --format=sparse query --dataset_id=${DATASET_NAME} -n 250000 --nouse_legacy_sql "${MISSING_FILE_SQL}" | tail -n+3 | tr -d "[:blank:]"`
+  transform "$MISSING_FILE_IDS" "missing files from attribute $2 in table $1"
+}
+
+function query_missing() {
+  MISSING_FILE_SQL="SELECT distinct($2) FROM $1 WHERE $2 NOT IN (SELECT file_id FROM file)"
+  MISSING_FILE_IDS=`bq --project_id=${PROJECT_ID} --format=sparse query --dataset_id=${DATASET_NAME} -n 250000 --nouse_legacy_sql "${MISSING_FILE_SQL}" | tail -n+3 | tr -d "[:blank:]"`
+  transform "${MISSING_FILE_IDS}" "missing files from attribute $2 in table $1"
+}
+
+
+query_missing_unnest "alignmentactivity" "generated_file_id"
+query_missing_unnest "alignmentactivity" "used_file_id"
+query_missing_unnest "analysisactivity" "derived_from_file_id"
+query_missing_unnest "analysisactivity" "generated_file_id"
+query_missing_unnest "assayactivity" "generated_file_id"
+query_missing_unnest "experimentactivity" "used_file_id"
+query_missing_unnest "experimentactivity" "generated_file_id"
+query_missing_unnest "file" "derived_from_file_id"
+query_missing "file" "paired_with_file_id"
+query_missing_unnest "referencefileset" "derived_from_file_id"
+query_missing_unnest "referencefileset" "generated_file_id"
+query_missing_unnest "referencefileset" "original_file_id"
+query_missing_unnest "referencefileset" "related_file_id"
+query_missing_unnest "sequencingactivity" "generated_file_id"
+query_missing_unnest "stepactivity" "used_file_id"
+query_missing_unnest "stepactivity" "generated_file_id"
 
 TOTAL_NUM=`wc -l ${TMP_FILE}`
 sort ${TMP_FILE} | uniq > ${OUTPUT_FILE}
